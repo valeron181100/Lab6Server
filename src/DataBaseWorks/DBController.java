9e84900aa5730106daee5b65749748ce3515fcf2
valeron181100 <bondar2501@outlook.com>
2019-05-23 02:46:57 +0300

    *Добавлены некоторые методы в контроллер бд

--- src/DataBaseWorks/DBController.java
+++ src/DataBaseWorks/DBController.java
@@ -2,6 +2,8 @@ package DataBaseWorks;
 
 import Clothes.Costume;
 
+import java.util.Collection;
+
 
 public class DBController {
 
@@ -16,9 +18,16 @@ public class DBController {
                 connector.execSQLUpdate(query));
     }
 
+    public void addAllCostumesToDB(Collection<Costume> costumes){
+        costumes.forEach(this::addCostumeToDB);
+    }
+
     public void removeCostumeFromDB(Costume costume){
         costume.getDelSQLQueries().forEach(query ->
                 connector.execSQLUpdate(query));
     }
 
+    public void removeAllCostumesFromDB(Collection<Costume> costumes){
+        costumes.forEach(this::removeCostumeFromDB);
+    }
 }
